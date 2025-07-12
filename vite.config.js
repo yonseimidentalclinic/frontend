323961b5d4d9391d6ed29a1a7559d6e014d0b064
gun hyun park <pghdent43@gmail.com>
2025-07-12 12:59:44 +0900

    ddd

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // 'mode'는 현재 실행 상태를 알려줍니다. (yarn dev 실행 시 'development')
+  // 현재 작업 디렉토리에서 .env 파일을 로드합니다.
+  const env = loadEnv(mode, process.cwd(), '');
   
   return {
     plugins: [react()],
     // 'define' 옵션을 사용해 코드 내의 특정 변수를 강제로 치환합니다.
     define: {
       // 코드에서 import.meta.env.VITE_API_URL을 만나면,
-      // 현재 모드가 'development'일 경우 'http://localhost:3001'로 강제 치환합니다.
-      // Vercel에서 빌드할 때는 Vercel의 환경 변수를 사용하게 됩니다.
-      'import.meta.env.VITE_API_URL': JSON.stringify(
-        mode === 'development'
-          ? 'http://localhost:3001'
-          : process.env.VITE_API_URL
-      ),
-    },
-  };
-});
+      // .env.local 파일에서 읽어온 VITE_API_URL 값(문자열)으로 바꿔치기합니다.
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    }
+  }
+})
