650c917ed2acc5a6d9e58cc8842f34f4188294d3
gun hyun park <pghdent43@gmail.com>
2025-07-18 21:45:05 +0900

    pppppppppppppp

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import DoctorsPage from './pages/DoctorsPage';
 import CasesPage from './pages/CasesPage';
-import FaqPage from './pages/FaqPage'; 
-// *** 핵심 수정: 존재하지 않는 InfoPage 관련 코드를 삭제했습니다. ***
-// import InfoPage from './pages/InfoPage'; 
+import FaqPage from './pages/FaqPage';
 import LocationPage from './pages/LocationPage';
 import NoticeListPage from './pages/NoticeListPage';
 import NoticeDetailPage from './pages/NoticeDetailPage';
@@ -26,22 +24,28 @@ import ConsultationListPage from './pages/ConsultationListPage';
 import ConsultationDetailPage from './pages/ConsultationDetailPage';
 import ConsultationWritePage from './pages/ConsultationWritePage';
 import ConsultationEditPage from './pages/ConsultationEditPage';
-import ReviewPage from './pages/ReviewPage';
 import ReservationPage from './pages/ReservationPage';
+// *** 수정: ReviewPage.jsx 파일이 없어 빌드 오류가 발생하므로 관련 코드를 주석 처리합니다. ***
+// import ReviewPage from './pages/ReviewPage';
 
-// 관리자용 페이지 컴포넌트 (모든 페이지 import)
+// 관리자용 페이지 컴포넌트 (스크린샷에서 확인된 파일만 활성화)
 import AdminLoginPage from './pages/admin/AdminLoginPage';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage';
 import AdminNoticeListPage from './pages/admin/AdminNoticeListPage';
-import AdminPostListPage from './pages/admin/AdminPostListPage';
 import AdminConsultationListPage from './pages/admin/AdminConsultationListPage';
-import AdminFaqListPage from './pages/admin/AdminFaqListPage';
-import AdminReviewListPage from './pages/admin/AdminReviewListPage';
-import AdminReservationListPage from './pages/admin/AdminReservationListPage';
 import AdminCasesListPage from './pages/admin/AdminCasesListPage';
-import AdminDoctorsListPage from './pages/admin/AdminDoctorsListPage';
+// *** 수정: 스크린샷의 실제 파일 이름(AdminDoctorListPage)에 맞게 수정했습니다. ***
+import AdminDoctorListPage from './pages/admin/AdminDoctorListPage';
 import AdminAboutPage from './pages/admin/AdminAboutPage';
-import AdminSchedulePage from './pages/admin/AdminSchedulePage';
+
+// --- 참고 ---
+// 아래 관리자 페이지 파일들은 아직 프로젝트에 없어 빌드 오류를 유발할 수 있습니다.
+// 향후 해당 페이지 파일을 생성하신 후, 이 import와 아래 Routes의 경로 주석을 해제하여 사용해주세요.
+// import AdminPostListPage from './pages/admin/AdminPostListPage';
+// import AdminFaqListPage from './pages/admin/AdminFaqListPage';
+// import AdminReviewListPage from './pages/admin/AdminReviewListPage';
+// import AdminReservationListPage from './pages/admin/AdminReservationListPage';
+// import AdminSchedulePage from './pages/admin/AdminSchedulePage';
 
 
 // 인증 상태에 따라 관리자 페이지 접근을 제어하는 컴포넌트
@@ -61,8 +65,6 @@ function App() {
           <Route path="doctors" element={<DoctorsPage />} />
           <Route path="cases" element={<CasesPage />} />
           <Route path="faq" element={<FaqPage />} />
-          {/* *** 핵심 수정: 존재하지 않는 InfoPage 라우트를 삭제했습니다. *** */}
-          {/* <Route path="info" element={<InfoPage />} /> */}
           <Route path="location" element={<LocationPage />} />
           <Route path="notices" element={<NoticeListPage />} />
           <Route path="notices/:id" element={<NoticeDetailPage />} />
@@ -74,8 +76,9 @@ function App() {
           <Route path="consultations/:id" element={<ConsultationDetailPage />} />
           <Route path="consultations/write" element={<ConsultationWritePage />} />
           <Route path="consultations/edit/:id" element={<ConsultationEditPage />} />
-          <Route path="reviews" element={<ReviewPage />} />
           <Route path="reservation" element={<ReservationPage />} />
+          {/* *** 수정: ReviewPage 라우트를 주석 처리합니다. *** */}
+          {/* <Route path="reviews" element={<ReviewPage />} /> */}
         </Route>
 
         {/* 관리자용 페이지 라우트 */}
@@ -90,16 +93,19 @@ function App() {
         >
           <Route index element={<Navigate to="dashboard" replace />} />
           <Route path="dashboard" element={<AdminDashboardPage />} />
-          <Route path="reservations" element={<AdminReservationListPage />} />
-          <Route path="schedule" element={<AdminSchedulePage />} />
           <Route path="about" element={<AdminAboutPage />} />
-          <Route path="doctors" element={<AdminDoctorsListPage />} />
+          <Route path="doctors" element={<AdminDoctorListPage />} />
           <Route path="cases" element={<AdminCasesListPage />} />
-          <Route path="reviews" element={<AdminReviewListPage />} />
-          <Route path="faqs" element={<AdminFaqListPage />} />
           <Route path="notices" element={<AdminNoticeListPage />} />
-          <Route path="posts" element={<AdminPostListPage />} />
           <Route path="consultations" element={<AdminConsultationListPage />} />
+          
+          {/* --- 참고 --- */}
+          {/* 아래 페이지들은 파일을 만드신 후 주석을 해제하여 경로를 연결해주세요. */}
+          {/* <Route path="reservations" element={<AdminReservationListPage />} /> */}
+          {/* <Route path="schedule" element={<AdminSchedulePage />} /> */}
+          {/* <Route path="reviews" element={<AdminReviewListPage />} /> */}
+          {/* <Route path="faqs" element={<AdminFaqListPage />} /> */}
+          {/* <Route path="posts" element={<AdminPostListPage />} /> */}
         </Route>
       </Routes>
     </Router>
